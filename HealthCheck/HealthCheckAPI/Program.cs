1196aeb2dcd26f0d8a6f7dde01fa1060c461dc09
Ppp5500 <pinkaulait@gmail.com>
2023-04-14 10:16:57 +0900

    create WorldCities and start set up(not complete)

--- HealthCheck/HealthCheckAPI/Program.cs
+++ HealthCheck/HealthCheckAPI/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHealthChecks()
     .AddCheck("ICMP_01", new ICMPHealthCheck("www.ryadel.com", 100))
     .AddCheck("ICMP_02", new ICMPHealthCheck("www.google.com", 100))
-    .AddCheck("ICMP_03", new ICMPHealthCheck($"www.{Guid.NewGuid():N}.com", 100));
+    .AddCheck("ICMP_03", new ICMPHealthCheck($"www.{Guid.NewGuid():N}.com", 100))
+    .AddCheck("ICMP_04", new ICMPHealthCheck("www.msdn.com", 100));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
