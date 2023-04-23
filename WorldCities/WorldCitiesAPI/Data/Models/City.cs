5ab27e1db22ef623b502abbffabd97974f7b69aa
Ppp5500 <pinkaulait@gmail.com>
2023-04-23 21:22:28 +0900

    add a picture in home-component, installed EF core Packages, installed mysql.Data Package,add City and Country class

---
+++ WorldCities/WorldCitiesAPI/Data/Models/City.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorldCitiesAPI.Data.Models;
+
+public class City
+{
+    #region Properties
+    /// <summary>
+    /// The Unique id and primary key for this City
+    /// </summary>
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    /// <summary>
+    /// City name (in UTF8 fotmat)
+    /// </summary>
+    public string Name { get; set; } = null!;
+    /// <summary>
+    /// City Latitude
+    /// </summary>
+    [Column(TypeName = "decimal(7, 4)")]
+    public decimal Lat { get; set; }
+    /// <summary>
+    /// City Longitude
+    /// </summary>
+    [Column(TypeName ="decimal(7, 4)")]
+    public decimal Lon {  get; set; }
+    /// <summary>
+    /// Country Id(foreign key
+    /// </summary>
+    public int CountryId { get; set; }
+    #endregion
+}
