c839790ef1af53eb6fb85fce497fa1d3f0bc8d58
Ppp5500 <pinkaulait@gmail.com>
2023-04-24 23:22:06 +0900

    installed mysql.entityframeworkcore, trying make connection(not yet work!)

---
+++ WorldCities/WorldCitiesAPI/Data/Models/BlogDataContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySql.Data;
+using MySql.EntityFrameworkCore;
+
+namespace WorldCitiesAPI.Data.Models;
+
+public class BlogDataContext : DbContext
+{
+    MySql.Data.MySqlClient.MySqlConnection conn;
+    static readonly string connectionString = "server=127.0.0.1;uid=root;pwd=azsx1324;database=my_database";
+
+    public DbSet<inform> informs { get; set; } = null!;
+
+    /*
+    public BlogDataContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    */
+
+    /*
+    public DbSet<Author>? Authors { get; set; }
+    public DbSet<Post>? Posts { get; set; }
+    */
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json")
+               .Build();
+            var connectionString = configuration.GetConnectionString("ConnectionString");
+            optionsBuilder.UseMySQL(connectionString);
+            Console.WriteLine($"conn opened!");
+        }
+    }
+}
+
+public class inform
+{
+    public int num { get; set; }
+    public string Uname { get; set; }
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public Author Author { get; set; }
+}
+
+public class Author
+{
+    public int AuthorId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public List<Post> Posts { get; set; }
+}
--- WorldCities/WorldCitiesAPI/Program.cs
+++ WorldCities/WorldCitiesAPI/Program.cs
@@ -1,11 +1,66 @@
+using WorldCitiesAPI.Data.Models;
+using MySql.Data;
+using MySql.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+Console.WriteLine($"We are in Program.cs");
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BlogDataContext>();
+Console.WriteLine($"We just added DbContext");
+
+using (var context = new BlogDataContext())
+{
+    /*
+    var john = new Author { Name = "John T. Author", Email = "john@example.com" };
+    context.Authors.Add(john);
+
+    var jane = new Author { Name = "Jane Q. Hacker", Email = "jane@example.com" };
+    context.Authors.Add(jane);
+
+    var post = new Post { Title = "Hello World", Content = "I wrote an app using EF Core!", Author = jane };
+    context.Posts.Add(post);
+    post = new Post { Title = "How to use EF Core", Content = "It's pretty easy", Author = john };
+    context.Posts.Add(post);
+    */
+
+    var john = new inform { num = 0};
+    context.informs.Add(john);
+
+    var tylor = new inform { num = 1};
+    context.informs.Add(tylor);
+
+    context.SaveChanges();
+}
+
+// query the blog posts, using a join between the two tables
+using (var context = new BlogDataContext())
+{
+    var infomation = context.informs
+        .ToList();
+
+    foreach( var name in infomation)
+    {
+        Console.WriteLine(name);
+    }
+    /*
+    var posts = context.Posts
+        .Include(p => p.Author)
+        .ToList();
+
+    foreach (var post in posts)
+    {
+        Console.WriteLine($"{post.Title} by {post.Author.Name}");
+    }
+    */
+}
+Console.WriteLine($"asdf");
 
 var app = builder.Build();
 
