10667ff436d0e31313a59176c99624bf6df7bc05
Ppp5500 <pinkaulait@gmail.com>
2023-04-25 23:40:32 +0900

    delete mysql packages

---
+++ WorldCities/WorldCitiesAPI/Data/Models/UserDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorldCitiesAPI.Data.Models;
+
+public class UserDataContext : DbContext
+{
+    public DbSet<User>? Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+}
+
+public class User
+{
+    public int UID { get; set; }
+    public string? Name { get; set; }
+}
